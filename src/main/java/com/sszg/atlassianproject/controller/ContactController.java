a793b2802c405f4c6d86b553f9428595ecf38c95
SaranSundar <saransszg@gmail.com>
2021-06-24 12:06:53 -0500

    Added PUT for contact

--- src/main/java/com/sszg/atlassianproject/controller/ContactController.java
+++ src/main/java/com/sszg/atlassianproject/controller/ContactController.java
@@ -3,7 +3,7 @@ package com.sszg.atlassianproject.controller;
 import com.amazonaws.services.kms.model.NotFoundException;
 import com.sszg.atlassianproject.exception.InvalidRequestException;
 import com.sszg.atlassianproject.model.Contact;
-import com.sszg.atlassianproject.service.VendorAccountService;
+import com.sszg.atlassianproject.service.ContactService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +16,10 @@ import java.util.List;
 @Slf4j
 public class ContactController {
 
-    private final VendorAccountService vendorAccountService;
+    private final ContactService contactService;
 
-    public ContactController(VendorAccountService vendorAccountService) {
-        this.vendorAccountService = vendorAccountService;
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
     }
 
     // HTTP POST URL - http://localhost:9500/api/contact
@@ -27,35 +27,43 @@ public class ContactController {
     public ResponseEntity<String> postContact(@RequestBody Contact contact) throws InvalidRequestException {
         log.info("Starting to post contact");
         log.info(contact.toString());
-        vendorAccountService.postContact(contact);
+        contactService.postContact(contact);
         return new ResponseEntity<>(contact.getUid(), HttpStatus.OK);
     }
 
-    // HTTP GET URL - http://localhost:9500/api/contact
+    // HTTP POST URL - http://localhost:9500/api/contact
+    @PutMapping("/contact")
+    public ResponseEntity<String> putContact(@RequestBody Contact contact) throws InvalidRequestException {
+        log.info("Starting to put contact");
+        log.info(contact.toString());
+        contactService.putContact(contact);
+        return new ResponseEntity<>(contact.getUid(), HttpStatus.OK);
+    }
+
+    // HTTP GET URL - http://localhost:9500/api/contact?uid=
     @GetMapping("/contact")
     public ResponseEntity<Contact> getContact(@RequestParam String uid) throws InvalidRequestException, NotFoundException {
         log.info("Starting to get contact");
         log.info(uid);
-        Contact contact = vendorAccountService.getContact(uid);
+        Contact contact = contactService.getContact(uid);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
 
-    // HTTP GET URL - http://localhost:9500/api/contact
-    @PutMapping("/contact")
+    // HTTP DELETE URL - http://localhost:9500/api/contact?uid=
+    @DeleteMapping("/contact")
     public ResponseEntity<Contact> deleteContact(@RequestParam String uid) throws InvalidRequestException, NotFoundException {
         log.info("Starting to delete contact");
         log.info(uid);
-        Contact contact = vendorAccountService.deleteContact(uid);
+        Contact contact = contactService.deleteContact(uid);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
 
-    // TODO: CHange get urls to match paramaters
     // HTTP GET URL - http://localhost:9500/api/contact-by-email?email=
     @GetMapping("/contact-by-email")
     public ResponseEntity<List<Contact>> getContactsByEmail(@RequestParam String email) throws InvalidRequestException, NotFoundException {
         log.info("Starting to query contact by email");
         log.info(email);
-        List<Contact> contacts = vendorAccountService.getContactsByEmail(email);
+        List<Contact> contacts = contactService.getContactsByEmail(email);
         return new ResponseEntity<>(contacts, HttpStatus.OK);
     }
 }
--- src/main/java/com/sszg/atlassianproject/service/ContactService.java
+++ src/main/java/com/sszg/atlassianproject/service/ContactService.java
@@ -9,38 +9,47 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class VendorAccountService {
+public class ContactService {
 
     private final ContactStore contactStore;
 
-    public VendorAccountService(ContactStore contactStore) {
+    public ContactService(ContactStore contactStore) {
         this.contactStore = contactStore;
     }
 
     public void postContact(Contact contact) {
         if (contact.getUid() != null) {
-            throw new InvalidRequestException("Contact for post should not provide UID");
+            throw new InvalidRequestException("Contact for POST should not provide UID");
         }
+        contactStore.saveContact(contact);
+    }
+
+    public void putContact(Contact contact) {
+        if (StringUtils.isNullOrEmpty(contact.getUid())) {
+            throw new InvalidRequestException("Contact for PUT should provide a UID");
+        }
+        getContact(contact.getUid());
+        // We try to get contact before saving new contact because, if it fails to get it will throw a ItemNotFoundException
         contactStore.saveContact(contact);
     }
 
     public Contact getContact(String uid) {
         if (StringUtils.isNullOrEmpty(uid)) {
-            throw new InvalidRequestException("Contact UID for get should is null or empty");
+            throw new InvalidRequestException("Contact UID for GET should is null or empty");
         }
         return contactStore.getContact(uid);
     }
 
     public Contact deleteContact(String uid) {
         if (StringUtils.isNullOrEmpty(uid)) {
-            throw new InvalidRequestException("Contact UID for get should is null or empty");
+            throw new InvalidRequestException("Contact UID for GET should is null or empty");
         }
         return contactStore.deleteContact(uid);
     }
 
     public List<Contact> getContactsByEmail(String email) {
         if (StringUtils.isNullOrEmpty(email)) {
-            throw new InvalidRequestException("Contact email for get is null or empty");
+            throw new InvalidRequestException("Contact email for GET is null or empty");
         }
         return contactStore.getAllContactsWithEmail(email);
     }
