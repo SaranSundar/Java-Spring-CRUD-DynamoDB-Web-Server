55c7ec578e94e017c77b698eb7ad073ee861bd2a
SaranSundar <saransszg@gmail.com>
2021-06-26 23:10:30 -0400

    Added more documentation response codes for contact controller

--- src/main/java/com/sszg/atlassianproject/controller/ContactController.java
+++ src/main/java/com/sszg/atlassianproject/controller/ContactController.java
@@ -4,6 +4,12 @@ import com.amazonaws.services.kms.model.NotFoundException;
 import com.sszg.atlassianproject.exception.InvalidRequestException;
 import com.sszg.atlassianproject.model.Contact;
 import com.sszg.atlassianproject.service.ContactService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +30,15 @@ public class ContactController {
     }
 
     // HTTP POST URL - http://localhost:9500/api/contact
+    @Operation(summary = "Create a new contact")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Created a new contact, returns back contact UID",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid contact object supplied",
+                    content = @Content)})
     @PostMapping("/contact")
-    public ResponseEntity<String> postContact(@Valid @RequestBody Contact contact) throws InvalidRequestException {
+    public ResponseEntity<String> postContact(@Parameter(description = "contact object that will be created") @Valid @RequestBody final Contact contact) throws InvalidRequestException {
         log.info("Starting to post contact");
         log.info(contact.toString());
         contactService.postContact(contact);
@@ -33,8 +46,17 @@ public class ContactController {
     }
 
     // HTTP POST URL - http://localhost:9500/api/contact
+    @Operation(summary = "Updates existing contact")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated existing contact, returns back contact UID",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid contact object supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "contact not found for given UID",
+                    content = @Content)})
     @PutMapping("/contact")
-    public ResponseEntity<String> putContact(@Valid @RequestBody Contact contact) throws InvalidRequestException {
+    public ResponseEntity<String> putContact(@Parameter(description = "contact object that will be created") @Valid @RequestBody final Contact contact) throws InvalidRequestException {
         log.info("Starting to put contact");
         log.info(contact.toString());
         contactService.putContact(contact);
@@ -42,8 +64,17 @@ public class ContactController {
     }
 
     // HTTP GET URL - http://localhost:9500/api/contact/{{contactUid}}
+    @Operation(summary = "Gets existing contact")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found existing contact, returns back contact",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Contact.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid contact object supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "contact not found for given UID",
+                    content = @Content)})
     @GetMapping("/contact/{contactUid}")
-    public ResponseEntity<Contact> getContact(@PathVariable String contactUid) throws InvalidRequestException, NotFoundException {
+    public ResponseEntity<Contact> getContact(@Parameter(description = "UID of existing contact") @PathVariable final String contactUid) throws InvalidRequestException, NotFoundException {
         log.info("Starting to get contact");
         log.info(contactUid);
         Contact contact = contactService.getContact(contactUid);
@@ -51,8 +82,17 @@ public class ContactController {
     }
 
     // HTTP DELETE URL - http://localhost:9500/api/contact/{{contactUid}}
+    @Operation(summary = "Deletes existing contact")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deletes existing contact, returns back deleted contact uid",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Contact.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid contact object supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "contact not found for given UID",
+                    content = @Content)})
     @DeleteMapping("/contact/{contactUid}")
-    public ResponseEntity<Contact> deleteContact(@PathVariable String contactUid) throws InvalidRequestException, NotFoundException {
+    public ResponseEntity<Contact> deleteContact(@Parameter(description = "UID of existing contact") @PathVariable final String contactUid) throws InvalidRequestException, NotFoundException {
         log.info("Starting to delete contact");
         log.info(contactUid);
         Contact contact = contactService.deleteContact(contactUid);
@@ -60,8 +100,13 @@ public class ContactController {
     }
 
     // HTTP GET URL - http://localhost:9500/api/contact?email=
+    @Operation(summary = "Finds all contacts with given email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Finds all contacts with given email, then returns back list of found contacts",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Contact.class))})})
     @GetMapping("/contact")
-    public ResponseEntity<List<Contact>> getContactsByEmail(@RequestParam String email) throws InvalidRequestException, NotFoundException {
+    public ResponseEntity<List<Contact>> getContactsByEmail(@Parameter(description = "email to query contacts by") @RequestParam final String email) throws InvalidRequestException, NotFoundException {
         log.info("Starting to query contact by email");
         log.info(email);
         List<Contact> contacts = contactService.getContactsByEmail(email);
